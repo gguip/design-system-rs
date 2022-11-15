90390842b4c4c952532514b9ecf7508c5741c84c
Guilherme Goomer <guilherme.passarinho@goomer.com.br>
2022-11-15 16:26:36 -0300

    fix some deploy error with msw

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -3,7 +3,16 @@ import { themes } from "@storybook/theming";
 
 import "../src/styles/global.css";
 
-initialize({
+let options = {};
+if (location.hostname === "gguip.github.io") {
+  options = {
+    serviceWorker: {
+      url: "/design-system-rs/mockServiceWorker.js",
+    },
+  };
+}
+
+initialize(options, {
   onUnhandledRequest: "bypass",
 });
 
